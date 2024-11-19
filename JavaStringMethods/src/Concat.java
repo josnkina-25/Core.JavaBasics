e4c6431a73473fc21165682e063aa9875ff4a721
josias-25 <josnkina@gmail.com>
2024-11-19 16:17:17 -0500

    Core Java

---
+++ JavaStringMethods/src/Concat.java
@@ -0,0 +1,28 @@
+public class Concat {
+    public static void main(String[] args) {
+      // ----By using concat method ---
+        String str1 = "Learn ";
+        String str2 = "Java";
+         // concatenate str1 and str2
+        System.out.println(str1.concat(str2)); // "Learn Java"
+
+        // concatenate str2 and str1
+        System.out.println(str2.concat(str1)); // "JavaLearn"
+
+        // ---By using + operator---
+        String s3 = "hello";
+        String s4 = "Learners";
+        // String s5 = s3.concat(s4); or
+        String s5 = s3 + s4;
+        // both of the above statement will give the same result
+
+        // Three strings are concatenated
+        String message = "Welcome " + "to " + "Java";
+
+        //String Chapter is cancatenated with number 2
+        String s = "Chapter" + 2; // s becomes chapter 2
+
+        // String Supplement is concatenated with character B
+        String s1 = "Supplement" + 'B'; // s1 becomes SupplementB
+    }
+}
---
+++ OperatorsAndNumbers/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // left Shift Operator
+        int x = 2;
+        System.out.println(x);
+
+
+    }
+}
