1dbc1526be8f099da9dfd4eabd3e099ab567c771
josias-25 <josnkina@gmail.com>
2024-12-01 21:21:27 -0500

    GLAB 303.5.1

---
+++ Guided LAB 303.5.1 for loop/src/loopDemoOne.java
@@ -0,0 +1,12 @@
+public class loopDemoOne {
+    public static void main(String[] args) {
+        // program to print a text 5 times
+        int n = 5;
+        // for loop
+        for (int i  = 1; i <= n; ++i){
+            System.out.println("Java is fun");
+
+        }
+    }
+
+}
